8f68420fdc3bb725edbb84c5a8d6fabe4c7a2f46
Fortune.Zhang <zhangfortune527@gmail.com>
2013-07-05 17:25:41 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PicAndVedio::Application.config.secret_token = '6245a3ef1b3a80507d71dd8909ec988782cb4165fa447f2e2c5e32481710161d1916188519afec298e34b6b022d65227a80915ac2a19c826b4bc4cd5ae34b83b'
